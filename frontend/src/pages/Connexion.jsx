e9cfbb2654fcc0b7367f0a7280b9555069bb7f4f
AnneClr <anneclaire.fougerit@gmail.com>
2023-06-20 17:21:24 +0200

    commit before pull dev

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -1,5 +1,39 @@
-function Connexion() {
-  return <p>Connexion</p>;
-}
+import { Link } from "react-router-dom";
 
+function Connexion() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/subscribe">S'inscrire</Link>
+          </li>
+          <li>
+            <Link to="/connexion">Se connecter</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <p>Je suis un candidat</p>
+        <img src="./images/candidat.jpg" alt="applicant" />
+      </div>
+
+      <div>
+        <p>Je suis un employeur</p>
+        <img src="./images/company.jpg" alt="company" />
+      </div>
+      <form
+        className="signin-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Mot de passe" />
+        <span>Mot de passe oublié?</span>
+        <button type="button">Je me connecte</button>
+      </form>
+    </>
+  );
+}
 export default Connexion;
