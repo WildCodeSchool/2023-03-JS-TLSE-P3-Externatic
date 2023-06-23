d443b10a4d0dca3672e8187b6e1a482b808ed037
NGBANDA_JUNIOR <njjngbanda20@yahoo.com>
2023-06-23 10:07:56 +0200

    commit before pull

--- backend/database.sql
+++ backend/database.sql
@@ -4,3 +4,13 @@ CREATE TABLE item (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+ CREATE TABLE admin (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(80) NOT NULL,
+  `lastname` VARCHAR(80) NOT NULL,
+  `mail` VARCHAR(100) NOT NULL,
+  `hashedPassword` VARCHAR(255) NOT NULL,
+   PRIMARY KEY (`id`)
+);
+
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,5 +1,177 @@
+import React, { useState } from "react";
+import "./MyProfile.css";
+import identificationBlack from "../assets/icons/identification_black.svg";
+import mailBlack from "../assets/icons/mail_black.svg";
+import mobileBlack from "../assets/icons/mobile_black.svg";
+import cityBlack from "../assets/icons/black_city.svg";
+import whiteTrash from "../assets/icons/white_trash.svg";
+import lockBlack from "../assets/icons/lock_black.svg";
+
 function MyProfile() {
-  return <p>MyProfile</p>;
+  const [genre, setGenre] = useState("");
+
+  return (
+    <div>
+      <div className="containerForm">
+        <h4 className="myProfil">Modifier mon profil</h4>
+        <div className="containerForm_2">
+          <form action=" " method="POST" className="allForm">
+            <div className="button-ratio">
+              <div className="groupRadio">
+                <label>
+                  <input
+                    type="radio"
+                    value="option1"
+                    name="genre"
+                    checked={genre === "option1"}
+                    onChange={() => setGenre("option1")}
+                  />
+                  Femme
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="option2"
+                    name="genre"
+                    checked={genre === "option2"}
+                    onChange={() => setGenre("option2")}
+                  />
+                  Homme
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="option3"
+                    name="genre"
+                    checked={genre === "option3"}
+                    onChange={() => setGenre("option3")}
+                  />
+                  Autre
+                </label>
+              </div>
+            </div>
+
+            <div className="input">
+              <img
+                className="iconForm"
+                src={identificationBlack}
+                alt="person"
+              />
+              <input
+                type="text"
+                placeholder="Nom"
+                name="nom"
+                className="inputForm"
+              />
+            </div>
+
+            <div className="input">
+              <img
+                className="iconForm"
+                src={identificationBlack}
+                alt="person"
+              />
+              <input
+                type="text"
+                placeholder="Prenom"
+                name="prenom"
+                className="inputForm"
+              />
+            </div>
+
+            <div className="input">
+              <img className="iconForm" src={mailBlack} alt="person" />
+              <input
+                type="email"
+                placeholder=" Email"
+                name="email"
+                className="inputForm"
+              />
+            </div>
+
+            <div className="input">
+              <img className="iconForm" src={lockBlack} alt="lock" />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                className="inputForm"
+              />
+            </div>
+
+            <div className="input">
+              <img className="iconForm" src={mobileBlack} alt="mobile" />
+              <input
+                type="tel"
+                placeholder="Téléphone"
+                name="telephone"
+                className="inputForm"
+              />
+            </div>
+
+            <div className="input">
+              <img className="iconForm" src={cityBlack} alt="city" />
+              <input
+                type="text"
+                placeholder="ville de residence"
+                name="localisation"
+                className="inputForm"
+              />
+              <small />
+            </div>
+
+            <button className="registerButton" type="submit">
+              J'enregistre
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="containerModif">
+        <h4 className="myProfil2">Modifier mon mot de passe</h4>
+
+        <div className="allForm">
+          <div className="input">
+            <img className="iconForm" src={lockBlack} alt="lock" />
+            <input
+              type="text"
+              placeholder="Ancien mot de passe"
+              name="ancienMdp"
+              className="inputForm"
+            />
+          </div>
+          <div className="input">
+            <img className="iconForm" src={lockBlack} alt="lock" />
+            <input
+              type="text"
+              placeholder="Nouveau mot de passe"
+              name="nouveauMdp"
+              className="inputForm"
+            />
+          </div>
+          <div className="input">
+            <img className="iconForm" src={lockBlack} alt="lock" />
+            <input
+              type="email"
+              placeholder="Confirmer mot de passe"
+              name="newMdp"
+              className="inputForm"
+            />
+          </div>
+          <button type="submit" className="registerButton">
+            J'enregistre
+          </button>
+
+          <div className="input2">
+            <button type="submit" className="deleteButton">
+              <img className="iconForm2" src={whiteTrash} alt="clean" />
+              Je supprime mon compte
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyProfile;
