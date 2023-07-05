9ff9bee5e7874a1c2d9a5ba11212b870d72a88bd
AnneClr <anneclaire.fougerit@gmail.com>
2023-07-05 14:31:19 +0200

    css & connexion.jsx modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home";
 import Connexion from "./pages/Connexion";
 import Subscribe from "./pages/Subscribe";
 import Offers from "./pages/Offers";
-import AdminConnexion from "./pages/AdminConnexion";
 import Dashboard from "./pages/Dashboard";
 
 // Import des composants
@@ -34,7 +33,6 @@ function App() {
                 <Route path="connexion" element={<Connexion />} />
                 <Route path="subscribe" element={<Subscribe />} />
                 <Route path="offers" element={<Offers />} />
-                <Route path="admin-connexion" element={<AdminConnexion />} />
                 <Route path="dashboard/*" element={<Dashboard />} />
               </Routes>
             </Router>
