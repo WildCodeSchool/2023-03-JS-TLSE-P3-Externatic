a502f98922ee53350bb7c501d976bac8990740f8
NGBANDA_JUNIOR <njjngbanda20@yahoo.com>
2023-07-06 10:43:50 +0200

    implementation of the 3 profiles: candidate, Admin and Company

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -204,6 +204,298 @@ function MyProfile() {
         />
         Je supprime mon compte
       </button>
+      {/* My_Profile Entreprise */}
+      <form onSubmit={handleSubmit} className="form">
+        <h2>Modifier mon profil</h2>
+        <div className="groupRadio">
+          <div className="containerRadioInput">
+            <label htmlFor="femmeInput" className="labelRadioInput">
+              Femme
+            </label>
+            <input
+              id="femmeInput"
+              className="radioInput"
+              type="radio"
+              value="option1"
+              name="genre"
+              defaultChecked
+            />
+          </div>
+          <div className="containerRadioInput">
+            <label htmlFor="hommeInput" className="labelRadioInput">
+              Homme
+            </label>
+            <input
+              id="hommeInput"
+              className="radioInput"
+              type="radio"
+              value="option2"
+              name="genre"
+            />
+          </div>
+          <div className="containerRadioInput">
+            <label htmlFor="autreInput" className="labelRadioInput">
+              Autre
+            </label>
+            <input
+              id="autreInput"
+              className="radioInput"
+              type="radio"
+              value="option3"
+              name="genre"
+            />
+          </div>
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={identificationBlack} alt="person" />
+          <input
+            type="text"
+            placeholder="Nom"
+            name="nom"
+            className="textInput"
+          />
+          {errors.nom && <small className="error">{errors.nom}</small>}
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={mailBlack} alt="person" />
+          <input
+            type="email"
+            placeholder=" Email"
+            name="email"
+            className="textInput"
+          />
+          {errors.email && <small className="error">{errors.email}</small>}
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={mobileBlack} alt="mobile" />
+          <div className="inputContainer">
+            <input
+              type="tel"
+              placeholder="Téléphone"
+              name="telephone"
+              onChange={(e) => validateTelephone(e.target.value)}
+              className="textInput"
+            />
+            {errors.telephone && <p className="error">{errors.telephone}</p>}
+          </div>
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={cityBlack} alt="city" />
+          <input
+            type="text"
+            placeholder="Ville"
+            name="localisation"
+            className="textInput"
+          />
+          {errors.localisation && (
+            <small className="error">{errors.localisation}</small>
+          )}
+        </div>
+
+        <button className="button" type="submit">
+          J'enregistre
+        </button>
+      </form>
+      <form className="form">
+        <h2>Modifier mon mot de passe</h2>
+        <div className="containerTextInput">
+          <img className="iconForm" src={lockBlack} alt="lock" />
+          <input
+            type="text"
+            placeholder="Ancien mot de passe"
+            name="ancienMdp"
+            className="textInput"
+          />
+        </div>
+        <div className="containerTextInput">
+          <img className="iconForm" src={lockBlack} alt="lock" />
+          <input
+            type="text"
+            placeholder="Nouveau mot de passe"
+            name="nouveauMdp"
+            className="textInput"
+          />
+          <small className="error">{errors.password}</small>
+        </div>
+        <div className="containerTextInput">
+          <img className="iconForm" src={lockBlack} alt="lock" />
+          <input
+            type="email"
+            placeholder="Confirmer mot de passe"
+            name="newMdp"
+            className="textInput"
+          />
+          <small className="error">{errors.password}</small>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            J'enregistre
+          </button>
+        </div>
+      </form>
+      <button type="button" className="button">
+        <img
+          className="iconForm2"
+          src={whiteTrash}
+          alt="Poubelle du bouton permettant de supprimer son compte"
+        />
+        Je supprime mon compte
+      </button>
+      {/* My_Profile Admin */}
+      <form onSubmit={handleSubmit} className="form">
+        <h2>Modifier mon profil</h2>
+        <div className="groupRadio">
+          <div className="containerRadioInput">
+            <label htmlFor="femmeInput" className="labelRadioInput">
+              Femme
+            </label>
+            <input
+              id="femmeInput"
+              className="radioInput"
+              type="radio"
+              value="option1"
+              name="genre"
+              defaultChecked
+            />
+          </div>
+          <div className="containerRadioInput">
+            <label htmlFor="hommeInput" className="labelRadioInput">
+              Homme
+            </label>
+            <input
+              id="hommeInput"
+              className="radioInput"
+              type="radio"
+              value="option2"
+              name="genre"
+            />
+          </div>
+          <div className="containerRadioInput">
+            <label htmlFor="autreInput" className="labelRadioInput">
+              Autre
+            </label>
+            <input
+              id="autreInput"
+              className="radioInput"
+              type="radio"
+              value="option3"
+              name="genre"
+            />
+          </div>
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={identificationBlack} alt="person" />
+          <input
+            type="text"
+            placeholder="Nom"
+            name="nom"
+            className="textInput"
+          />
+          {errors.nom && <small className="error">{errors.nom}</small>}
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={identificationBlack} alt="person" />
+          <input
+            type="text"
+            placeholder="Prénom"
+            name="prenom"
+            className="textInput"
+          />
+          {errors.prenom && <small className="error">{errors.prenom}</small>}
+        </div>
+        <div className="containerTextInput">
+          <img className="iconForm" src={mailBlack} alt="person" />
+          <input
+            type="email"
+            placeholder=" Email"
+            name="email"
+            className="textInput"
+          />
+          {errors.email && <small className="error">{errors.email}</small>}
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={mobileBlack} alt="mobile" />
+          <div className="inputContainer">
+            <input
+              type="tel"
+              placeholder="Téléphone"
+              name="telephone"
+              onChange={(e) => validateTelephone(e.target.value)}
+              className="textInput"
+            />
+            {errors.telephone && <p className="error">{errors.telephone}</p>}
+          </div>
+        </div>
+
+        <div className="containerTextInput">
+          <img className="iconForm" src={cityBlack} alt="city" />
+          <input
+            type="text"
+            placeholder="Ville"
+            name="localisation"
+            className="textInput"
+          />
+          {errors.localisation && (
+            <small className="error">{errors.localisation}</small>
+          )}
+        </div>
+
+        <button className="button" type="submit">
+          J'enregistre
+        </button>
+      </form>
+      <form className="form">
+        <h2>Modifier mon mot de passe</h2>
+        <div className="containerTextInput">
+          <img className="iconForm" src={lockBlack} alt="lock" />
+          <input
+            type="text"
+            placeholder="Ancien mot de passe"
+            name="ancienMdp"
+            className="textInput"
+          />
+        </div>
+        <div className="containerTextInput">
+          <img className="iconForm" src={lockBlack} alt="lock" />
+          <input
+            type="text"
+            placeholder="Nouveau mot de passe"
+            name="nouveauMdp"
+            className="textInput"
+          />
+          <small className="error">{errors.password}</small>
+        </div>
+        <div className="containerTextInput">
+          <img className="iconForm" src={lockBlack} alt="lock" />
+          <input
+            type="email"
+            placeholder="Confirmer mot de passe"
+            name="newMdp"
+            className="textInput"
+          />
+          <small className="error">{errors.password}</small>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            J'enregistre
+          </button>
+        </div>
+      </form>
+      <button type="button" className="button">
+        <img
+          className="iconForm2"
+          src={whiteTrash}
+          alt="Poubelle du bouton permettant de supprimer son compte"
+        />
+        Je supprime mon compte
+      </button>
     </div>
   );
 }
