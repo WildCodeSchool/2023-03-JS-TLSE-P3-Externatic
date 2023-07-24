6363f45c915032103360abbf8a6253dd6a35d81a
AnneClr <anneclaire.fougerit@gmail.com>
2023-07-24 16:15:13 +0200

    user connected has a pesonalized message on page home, style of description modified

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 
-// import OfferCardLarge from "../components/OfferCardLarge";
+// Import des components
+import OfferCardLarge from "../components/OfferCardLarge";
 import OfferModal from "../components/OfferModal";
 
 // Import du context
@@ -15,12 +16,23 @@ import rocketPink from "../assets/icons/rocket_pink.svg";
 import imageHeader from "../assets/images/header_image.svg";
 
 function Home() {
+  const [offersList, setOffersList] = useState([]);
   const [modalOfferIsOpen, setModalOfferIsOpen] = useState(false);
   const [selectedOffer, setSelectedOffer] = useState({});
   const [dataUser, setDataUser] = useState();
   const { userToken, userId, userRole } = useContext(TokenContext);
   console.info(setSelectedOffer);
 
+  const handleOpenModalOffer = (offerId) => {
+    const findOffer = offersList.find((offer) => offer.id === offerId);
+    setSelectedOffer(findOffer);
+    setModalOfferIsOpen(true);
+  };
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/offers`)
+      .then((results) => setOffersList(results.data));
+  }, []);
   useEffect(() => {
     if (userId && userToken) {
       axios
@@ -63,47 +75,79 @@ function Home() {
           {userRole === "company" ? dataUser?.name : dataUser?.firstname} !{" "}
         </h1>
       ) : (
-        <h1 className="userNotConnected">Bienvenue ! </h1>
+        <h1 className="userNotConnected">
+          Bienvenue sur Externatic <br />
+          Votre cabinet de recrutement informatique{" "}
+        </h1>
       )}
-      <div className="descriptionHome">
-        <div className="itemtext">
-          <h2>La réussite de notre cabinet de recrutement informatique ?</h2>
-          <div className="TextIcon">
-            <img className="iconForm" src={externaticHello} alt="proximite" />
-            <h3>Notre proximité</h3>
+      <div className="offersListCardLargeContainer">
+        {offersList.length ? (
+          offersList.map((offer) => (
+            <OfferCardLarge
+              key={offer.id}
+              offer={offer}
+              modalOfferIsOpen={modalOfferIsOpen}
+              setModalOfferIsOpen={setModalOfferIsOpen}
+              onCardClick={handleOpenModalOffer}
+            />
+          ))
+        ) : (
+          <div className="globalContainer">
+            <h3 className="errorTitle">Pas de résultat</h3>
           </div>
-          <p>
-            L'expérience professionnelle est une chose. L'expérience de vie en
-            est une autre. Alors nos consultants prennent le temps de faire
-            connaissance avec chaque personne, pour comprendre le contexte, le
-            parcours, les envies et les projets.
-          </p>
-        </div>
-        <div className="ItemText">
-          <div className="TextIcon">
-            <img className="iconForm" src={rocketPink} alt="performance" />
-            <h3>Notre performance</h3>
+        )}
+      </div>
+      <section className="descriptionHomePage">
+        <h1 className="titleDescription">
+          La réussite de notre cabinet de recrutement informatique ?
+        </h1>
+        <div className="containerDescription">
+          <div className="aboutExternatic">
+            <div className="textAndIcon">
+              <img
+                className="icon hand"
+                src={externaticHello}
+                alt="icone main"
+              />
+              <h2>Notre proximité</h2>
+            </div>
+            <p className="textDescription">
+              L'expérience professionnelle est une chose. L'expérience de vie en
+              est une autre. Alors nos consultants prennent le temps de faire
+              connaissance avec chaque personne, pour comprendre le contexte, le
+              parcours, les envies et les projets.
+            </p>
           </div>
-          <p>
-            Notre réseau est une force et nous y travaillons sans relâche. Notre
-            expérience nous permet d'identifier les vrais besoins d'une
-            entreprise et de ceux qui la rejoignent.
-          </p>
+          <div className="aboutExternatic">
+            <div className="textAndIcon">
+              <img className="icon rocket" src={rocketPink} alt="icone fusée" />
+              <h2>Notre performance</h2>
+            </div>
+            <p className="textDescription">
+              Notre réseau est une force et nous y travaillons sans relâche.
+              Notre expérience nous permet d'identifier les vrais besoins d'une
+              entreprise et de ceux qui la rejoignent.
+            </p>
+          </div>
+          <div className="aboutExternatic">
+            <div className="textAndIcon">
+              <img
+                className="icon hourglass"
+                src={externaticSablier}
+                alt="icone sablier"
+              />
+              <h2>Notre durabilité</h2>
+            </div>
+            <p className="textDescription">
+              Notre challenge est de trouver l'équipe qui fonctionnera ensemble
+              de manière professionnelle et personnelle, pour aller jusqu'au
+              bout d'un projet commun. Notre responsabilité vis-à-vis des
+              impacts de nos décisions et nos actions sur le long terme
+              correspond également à notre politique RSE.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="itemtext">
-        <div className="TextIcon">
-          <img className="iconForm" src={externaticSablier} alt="durabilite" />
-          <h3>Notre durabilité</h3>
-        </div>
-        <p>
-          Notre challenge est de trouver l'équipe qui fonctionnera ensemble de
-          manière professionnelle et personnelle, pour aller jusqu'au bout d'un
-          projet commun. Notre responsabilité vis-à-vis des impacts de nos
-          décisions et nos actions sur le long terme correspond également à
-          notre politique RSE.
-        </p>
-      </div>
+      </section>
     </>
   );
 }
